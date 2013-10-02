b630a05f8e961cb74554aefa6f6a79c8aacf7efe
Mike Judge <mikelovesrobots@gmail.com>
2013-10-02 15:13:15 -0700

    UIImages are now created with a default retina scale

--- AsyncImageView/AsyncImageView.m
+++ AsyncImageView/AsyncImageView.m
@@ -44,6 +44,7 @@ NSString *const AsyncImageURLKey = @"URL";
 NSString *const AsyncImageCacheKey = @"cache";
 NSString *const AsyncImageErrorKey = @"error";
 
+CGFloat const AsyncImageRetinaScale = 2.0;
 
 @interface AsyncImageConnection : NSObject
 
@@ -178,7 +179,7 @@ NSString *const AsyncImageErrorKey = @"error";
 	{	
 		if (!_cancelled)
 		{
-            UIImage *image = [[UIImage alloc] initWithData:data];
+			UIImage *image = [UIImage imageWithData:data scale:AsyncImageRetinaScale];
 			if (image)
 			{
 				//add to cache (may be cached already but it doesn't matter)
