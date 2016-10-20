cb3ef030ed1543472cb438c0ec24c381fb1ae22c
Juan Fernández Sagasti <juan@theagilemonkeys.com>
2016-10-20 14:26:10 +0100

    Added Carthage support

---
+++ AsyncImageViewFramework/AsyncImageViewFramework/AsyncImageViewFramework.h
@@ -0,0 +1,18 @@
+//
+//  AsyncImageViewFramework.h
+//  AsyncImageViewFramework
+//
+//  Created by Juan Fernández Sagasti on 20/10/16.
+//  Copyright © 2016 Juan Fernández Sagasti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncImageViewFramework.
+FOUNDATION_EXPORT double AsyncImageViewFrameworkVersionNumber;
+
+//! Project version string for AsyncImageViewFramework.
+FOUNDATION_EXPORT const unsigned char AsyncImageViewFrameworkVersionString[];
+
+#import <AsyncImageViewFramework/AsyncImageView.h>
+
